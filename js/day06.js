23c0dd3c41bc098b216d21ae65319b92ec280e7c
Bernhard Kerbl <kerbl@icg.tugraz.at>
2021-12-07 00:53:06 +0100

    Added day 6

--- js/day06.js
+++ js/day06.js
@@ -1,13 +1,12 @@
 function fishForDays(fishDays, numDays, cycle, delay) {
-    let addedPerDay = Array.from(new Array(cycle + delay), (_, i) => (i < delay ? 0 : fishDays.reduce((sum, d) => sum + (d + 1 == i ? 1 : 0), 0)));
+    let spawned = Array.from(new Array(cycle + delay), (_, i) => (i < delay ? 0 : fishDays.reduce((sum, d) => sum + (d + 1 == i ? 1 : 0), 0)));
     let schools = [0];
     for (let day = (cycle + delay); day <= numDays; day++) {
         if (day % cycle == delay)
             schools.push(day);
-        addedPerDay[day] = addedPerDay[day % cycle];
-        addedPerDay[day] += schools.slice(1).reduce((sum, spawned) => sum + addedPerDay[day - spawned], 0);
+        spawned[day] = spawned[day % cycle] + schools.slice(1).reduce((sum, s) => sum + spawned[day - s], 0);
     }
-    return addedPerDay.slice(0, numDays + 1).reduce((sum, x) => sum + x, fishDays.length);
+    return spawned.slice(0, numDays + 1).reduce((sum, x) => sum + x, fishDays.length);
 }
 function solve_part1(input) {
     const fishDays = input.split(',').map(x => parseInt(x));
--- ts/day06.ts
+++ ts/day06.ts
@@ -1,16 +1,15 @@
 function fishForDays ( fishDays: number[], numDays: number, cycle: number, delay: number )
 {
-    let addedPerDay: number[] = Array.from( new Array( cycle + delay ), ( _, i ) =>
+    let spawned: number[] = Array.from( new Array( cycle + delay ), ( _, i ) =>
         ( i < delay ? 0 : fishDays.reduce( ( sum, d ) => sum + ( d + 1 == i ? 1 : 0 ), 0 ) )
     )
     let schools: number[] = [ 0 ]
     for ( let day: number = ( cycle + delay ); day <= numDays; day++ )
     {
         if ( day % cycle == delay ) schools.push( day )
-        addedPerDay[ day ] = addedPerDay[ day % cycle ]
-        addedPerDay[ day ] += schools.slice( 1 ).reduce( ( sum, spawned ) => sum + addedPerDay[ day - spawned ], 0 )
+        spawned[ day ] = spawned[ day % cycle ] + schools.slice( 1 ).reduce( ( sum, s ) => sum + spawned[ day - s ], 0 )
     }
-    return addedPerDay.slice( 0, numDays + 1 ).reduce( ( sum, x ) => sum + x, fishDays.length )
+    return spawned.slice( 0, numDays + 1 ).reduce( ( sum, x ) => sum + x, fishDays.length )
 }
 
 function solve_part1 ( input: string ): string
