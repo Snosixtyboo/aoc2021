2124e9860b531d7ef38ecae0c4ab1a897401aabb
Bernhard Kerbl <kerbl@icg.tugraz.at>
2021-12-06 17:07:08 +0100

    Added day 6

--- js/day05.js
+++ js/day05.js
@@ -4,34 +4,30 @@ class Point {
 }
 class Segment {
     constructor(arr) {
-        this.points = [];
-        this.points = [new Point(arr[0], arr[1]), new Point(arr[2], arr[3])];
-        Object.seal(this.points);
+        this.p0 = new Point(arr[0], arr[1]);
+        this.p1 = new Point(arr[2], arr[3]);
     }
 }
 function countIntersections(lines, filterFunc) {
-    const lineToSegment = line => (a => new Segment(a.map(x => parseInt(x))))(line.split(/ -> |,/));
+    const lineToSegment = line => new Segment(line.split(/ -> |,/).map(x => parseInt(x)));
     const segments = lines.map(line => lineToSegment(line)).filter(filterFunc);
-    const dim = segments.reduce((prev, s) => (new Point(Math.max(prev.x, Math.max(s.points[0].x + 1, s.points[1].x + 1)), Math.max(prev.y, Math.max(s.points[0].y + 1, s.points[1].y + 1)))), new Point(0, 0));
+    const dim = segments.reduce((prev, segment) => (new Point(Math.max(prev.x, Math.max(segment.p0.x + 1, segment.p1.x + 1)), Math.max(prev.y, Math.max(segment.p0.y + 1, segment.p1.y + 1)))), new Point(0, 0));
     let intersections = 0;
     const field = Array.from(Array(dim.y), () => new Array(dim.x).fill(0));
     for (const segment of segments) {
-        const start = segment.points[0], stop = segment.points[1];
-        const dir = new Point(stop.x - start.x, stop.y - start.y);
+        const dir = new Point(segment.p1.x - segment.p0.x, segment.p1.y - segment.p0.y);
         const step = new Point(dir.x < 0 ? -1 : dir.x > 0 ? 1 : 0, dir.y < 0 ? -1 : dir.y > 0 ? 1 : 0);
-        for (let curr = start; !curr.is(stop); curr.x += step.x, curr.y += step.y)
+        for (let curr = segment.p0; !curr.is(segment.p1); curr.x += step.x, curr.y += step.y)
             if (++field[curr.y][curr.x] == 2)
                 intersections++;
-        if (++field[stop.y][stop.x] == 2)
+        if (++field[segment.p1.y][segment.p1.x] == 2)
             intersections++;
     }
     return intersections;
 }
 function solve_part1(input) {
     const lines = input.split('\n');
-    const result = countIntersections(lines, (segment) => {
-        return segment.points[1].y == segment.points[0].y || segment.points[1].x == segment.points[0].x;
-    });
+    const result = countIntersections(lines, s => s.p1.y == s.p0.y || s.p1.x == s.p0.x);
     return result.toString();
 }
 function solve_part2(input) {
