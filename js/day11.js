c49adc6f4094cee38a1ba4bc9d58ddaf10953862
Bernhard Kerbl <kerbl@icg.tugraz.at>
2021-12-11 17:51:05 +0100

    Added day 11

--- js/day11.js
+++ js/day11.js
@@ -1,7 +1,7 @@
-function flashOctopi(lines, pred) {
+function simulateOctopi(lines, pred) {
     const octopi = lines.map(line => line.split('').map(x => parseInt(x)));
-    const state = { it: 0, lastFlashes: 0, allFlashes: 0 };
-    for (state.it = 0; !pred(state); state.it++) {
+    const state = { phase: 0, lastFlashes: 0, allFlashes: 0 };
+    for (state.phase = 0; !pred(state.phase, state.lastFlashes); state.phase++) {
         let toUpdate = new Set();
         octopi.forEach((row, y) => row.forEach((_, x) => {
             if (++row[x] == 10)
@@ -9,12 +9,10 @@ function flashOctopi(lines, pred) {
         }));
         state.lastFlashes = 0;
         while (toUpdate.size > 0) {
-            let toUpdateNext = new Set();
+            const toUpdateNext = new Set();
             for (const p of toUpdate) {
                 for (let y = Math.max(0, p[1] - 1); y < Math.min(octopi.length, p[1] + 2); y++) {
                     for (let x = Math.max(0, p[0] - 1); x < Math.min(octopi[y].length, p[0] + 2); x++) {
-                        if (x == p[0] && y == p[1])
-                            continue;
                         if (++octopi[y][x] == 10)
                             toUpdateNext.add([x, y]);
                     }
@@ -34,13 +32,13 @@ function flashOctopi(lines, pred) {
 }
 function solve_part1(input) {
     const lines = input.split('\n');
-    const end = flashOctopi(lines, (state) => state.it == 100);
+    const end = simulateOctopi(lines, (phase, lastFlashes) => phase == 100);
     return end.allFlashes.toString();
 }
 function solve_part2(input) {
     const lines = input.split('\n');
-    const end = flashOctopi(lines, (state) => state.lastFlashes == 100);
-    return end.it.toString();
+    const end = simulateOctopi(lines, (phase, lastFlashes) => lastFlashes == 100);
+    return end.phase.toString();
 }
 // EOC
 export { solve_part1, solve_part2 };
--- ts/day11.ts
+++ ts/day11.ts
@@ -1,8 +1,9 @@
-function flashOctopi ( lines: readonly string[], pred: ( state: { it: number, lastFlashes: number, allFlashes: number } ) => boolean )
+function simulateOctopi ( lines: readonly string[], pred: ( phase: number, lastFlashes: number ) => boolean )
 {
     const octopi = lines.map<number[]>( line => line.split( '' ).map( x => parseInt( x ) ) )
-    const state = { it: 0, lastFlashes: 0, allFlashes: 0 }
-    for ( state.it = 0; !pred( state ); state.it++ )
+
+    const state = { phase: 0, lastFlashes: 0, allFlashes: 0 }
+    for ( state.phase = 0; !pred( state.phase, state.lastFlashes ); state.phase++ )
     {
         let toUpdate = new Set<readonly [ number, number ]>()
         octopi.forEach( ( row, y ) => row.forEach( ( _, x ) => 
@@ -14,15 +15,13 @@ function flashOctopi ( lines: readonly string[], pred: ( state: { it: number, la
         state.lastFlashes = 0
         while ( toUpdate.size > 0 )
         {
-            let toUpdateNext = new Set<readonly [ number, number ]>()
+            const toUpdateNext = new Set<readonly [ number, number ]>()
             for ( const p of toUpdate )
             {
                 for ( let y = Math.max( 0, p[ 1 ] - 1 ); y < Math.min( octopi.length, p[ 1 ] + 2 ); y++ )
                 {
                     for ( let x = Math.max( 0, p[ 0 ] - 1 ); x < Math.min( octopi[ y ].length, p[ 0 ] + 2 ); x++ )
                     {
-                        if ( x == p[ 0 ] && y == p[ 1 ] )
-                            continue
                         if ( ++octopi[ y ][ x ] == 10 )
                             toUpdateNext.add( [ x, y ] )
                     }
@@ -46,15 +45,15 @@ function flashOctopi ( lines: readonly string[], pred: ( state: { it: number, la
 function solve_part1 ( input: string ): string
 {
     const lines: readonly string[] = input.split( '\n' )
-    const end = flashOctopi( lines, ( state: { it: number, lastFlashes: number, allFlashes: number } ) => state.it == 100 )
+    const end = simulateOctopi( lines, ( phase: number, lastFlashes: number ) => phase == 100 )
     return end.allFlashes.toString()
 }
 
 function solve_part2 ( input: string ): string
 {
     const lines: readonly string[] = input.split( '\n' )
-    const end = flashOctopi( lines, ( state: { it: number, lastFlashes: number, allFlashes: number } ) => state.lastFlashes == 100 )
-    return end.it.toString()
+    const end = simulateOctopi( lines, ( phase: number, lastFlashes: number ) => lastFlashes == 100 )
+    return end.phase.toString()
 }
 // EOC
 
